0d52a8dd53fe3ff9fc2b420ca093f5d5e70b5178
Felix Slothower <little.lamb@users.noreply.github.com>
2020-11-02 08:32:03 -0800

    First upload

---
+++ John_Cock_Interpolator.py
@@ -0,0 +1,104 @@
+from os import system, name
+import pandas as pd
+import numpy as np
+
+def clearScreen():
+	if name == 'nt':
+		_ = system('cls') #for windows
+	else:
+		_ = system('clear') #fow mac
+
+def interpolate(x1,x2,y1,y2,inVal):
+	m = (y2-y1)/(x2-x1)
+	b = y2 - (m * x2)
+	outVal = (m * inVal) + b
+	return outVal
+
+def findNeighbours(inVar, inVal):
+	exactMatch = airProperties[airProperties[inVar] == inVal]
+	if not exactMatch.empty:
+		return exactMatch.index
+	else:
+		lowerVal = airProperties[airProperties[inVar]<inVal][inVar].idxmax()
+		upperVal = airProperties[airProperties[inVar]>inVal][inVar].idxmin()
+		#lowerVal_loc = airProperties[airProperties[inVar] == lowerVal].index.values
+		#upperVal_loc = airProperties[airProperties[inVar] == upperVal].index.values
+		return lowerVal, upperVal
+
+
+#start main loop
+clearScreen()
+print('\n\n' + '  ###  The John Cock Interpolator  ###' + '\n')
+
+#figuring out what table to download. SI or english.
+unitsLoop = True
+while unitsLoop:
+	try:
+		units = int(input('Enter the desired units;' + '\n\n' + 
+					  '1) SI' + '\n' + 
+					  '2) English' + '\n\n' + 
+					  '1 or 2: '))
+	except ValueError:
+		clearScreen()
+		print('\nThat\'s not even a number lol\n')
+		continue
+
+	if units == 1:
+		airProperties = pd.read_table('table_a22_SI.txt', sep='\t')
+		unitsLoop = False
+		clearScreen()
+	elif units == 2:
+		airProperties = pd.read_table('table_a22_En.txt', sep='\t')
+		unitsLoop = False
+		clearScreen()
+	else:
+		clearScreen()
+		print('\nIt\'s a 1 or 2 dude. Not that hard.\n')
+		continue
+
+while True:
+
+	#asking user to input the value from which we will interpolate
+	request = input('Enter the variable followed by its value (space delimited)\n' + 
+					'Accepted variables: [' +
+					', '.join(airProperties.columns.values) + ']\n\n')
+
+	#splitting the entry up into variable and value
+	requests = request.split()
+	inVar, inVal = str(requests[0]), float(requests[1])
+
+	#testing whether they entered a valid variable
+	try:
+		airProperties[inVar]
+	except IndexError:
+		clearScreen()
+		print('\nYou failed to enter an accepted variable. You fool. Try Again.\n')
+		continue
+
+	print(inVar, inVal)
+
+	values = []
+	x1_loc, x2_loc = findNeighbours(inVar, inVal)
+	x1 = airProperties[inVar][x1_loc]
+	x2 = airProperties[inVar][x2_loc]
+	for outVar in airProperties.columns.values:
+		y1 = airProperties.iloc[x1_loc][outVar]
+		y2 = airProperties.iloc[x2_loc][outVar]
+		outVal = interpolate(x1,x2,y1,y2,inVal)
+		tempList = [[outVar, outVal]]
+		values = values + tempList
+
+	print('\n')
+	for i in range(len(values)):
+		print(str(values[i][0]) + ' ' + str(values[i][1]))
+	print('\n')
+
+	closingRemarks = input('Press \"Enter\" to find another value.\n' +
+						   'Press \"Ctrl-C\" to quit... ')
+
+	if closingRemarks == '':
+		clearScreen()
+		continue
+	else:
+		clearScrean()
+		break 
